35d1ec1c1fb0bbb0a0b0d54420e4e3fc7b16b865
Shashidhar <shashidhar.4454@gmail.com>
2024-04-12 12:43:47 +0530

    commiting

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.management.MBeanAttributeInfo;
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+    }
+}
